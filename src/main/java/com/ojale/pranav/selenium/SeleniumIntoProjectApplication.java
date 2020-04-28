1be05acbbfbd332202cef02c6f2b40f12e99d71c
pranavoj <pranavojale@gmail.com>
2020-04-28 14:41:52 +0530

    Initial Commit

---
+++ src/main/java/com/ojale/pranav/selenium/SeleniumIntoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ojale.pranav.SeleniumIntoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumIntoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumIntoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ojale/pranav/SeleniumIntoProject/SeleniumIntoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojale.pranav.SeleniumIntoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumIntoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
